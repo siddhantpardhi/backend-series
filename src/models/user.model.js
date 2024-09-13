074b50d557313c6b2fec58ad1e5a5d1d521a8579
siddhantpardhi <siddhantpardhi10@gmail.com>
2024-09-13 17:34:44 +0530

    update user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             unique: true,
             trim: true
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -57,6 +57,35 @@ const userSchema = new Schema(
 
     userSchema.methods.isPasswordCorrect = async function(password) {
         return await bcrypt.compare(password, this.password)
+    }
+
+    userSchema.methods.generateAccessToken = function() {
+        return jwt.sign(
+            {
+                _id: this._id,
+                email: this.email,
+                username: this.username,
+                fullName: this.fullName,    
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            }
+        )
+    }
+    userSchema.methods.generateRefreshToken = function() {
+        return jwt.sign(
+            {
+                _id: this._id,
+                email: this.email,
+                username: this.username,
+                fullName: this.fullName,    
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            }
+        )
     }
 
     export const User = mongoose.model("User", userSchema)
